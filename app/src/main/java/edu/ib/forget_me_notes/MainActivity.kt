4360d3202a47ea1163368f956f7e078f25fe7329
Maki1502 <marcelina.karasek@gmail.com>
2022-03-25 16:04:52 +0100

    First fragment layout

--- app/src/main/java/edu/ib/forget_me_notes/MainActivity.kt
+++ app/src/main/java/edu/ib/forget_me_notes/MainActivity.kt
@@ -9,41 +9,35 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import edu.ib.forget_me_notes.Fragments.*
 import edu.ib.forget_me_notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textView: TextView
-
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textView.setText("Home")
-                //moveToFragment(HomeFragment())
+                moveToFragment(HomeFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.navigation_search -> {
-                textView.setText("Search")
-                //moveToFragment(SearchFragment())
+                moveToFragment(SearchFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.navigation_add -> {
-                textView.setText("Add")
-                //moveToFragment(AddFragment())
+                moveToFragment(AddFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.navigation_notifications -> {
-                textView.setText("Notifications")
-                //moveToFragment(NotificationFragment())
+                moveToFragment(NotificationsFragment())
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.navigation_settings -> {
-                textView.setText("Settings")
-                //moveToFragment(SettingsFragment())
+                moveToFragment(SettingsFragment())
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -55,16 +49,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        textView = findViewById(R.id.message)
 
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        //moveToFragment(HomeFragment())
+        moveToFragment(HomeFragment())
     }
 
-//    private fun moveToFragment(fragment: Fragment) {
-//        val fragmentTrans = supportFragmentManager.beginTransaction()
-//        fragmentTrans.replace(R.id.fragment_container, fragment)
-//        fragmentTrans.commit()
-//    }
+    private fun moveToFragment(fragment: Fragment) {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
+    }
 }
